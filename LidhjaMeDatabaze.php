c38c13a5e2cd1068fba73dee7dafd67ffced89a2
medi <beqiriabdusamed@gmail.com>
2023-11-24 11:35:38 +0100

    small changes in php

--- LidhjaMeDatabaze.php
+++ LidhjaMeDatabaze.php
@@ -1,5 +1,6 @@
 <?php
-$mysqli = new mysqli("localhost", "root", "", "autosalloni");
+
+$mysqli = @new mysqli('localhost', 'visitor','Muunjal2', 'autosalloni');
 
 if ($mysqli -> connect_errno) {
   die("Connection error: " . $mysqli -> connect_error);
